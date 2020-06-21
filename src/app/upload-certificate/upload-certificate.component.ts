da6b958d8f36804aff3b61b9a97178878da97017
BadrAziz <44317349+Badraziz@users.noreply.github.com>
2020-06-21 18:56:38 +0300

    First commit

---
+++ src/app/upload-certificate/upload-certificate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-certificate',
+  templateUrl: './upload-certificate.component.html',
+  styleUrls: ['./upload-certificate.component.css']
+})
+export class UploadCertificateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
